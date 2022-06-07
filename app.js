b50085262056e40cf816e7bde67ed416d760c68f
Krut Rupera <krutrupera74@gmail.com>
2022-06-07 10:10:47 +0530

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const _ = require('lodash')
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/temp.txt', 'THIS IS THE TEMP FILE \n', {flag: 'a'});
+        console.log(first);
+        console.log(second);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+
+//             }
+//         })
+//     })
+// }
+// getText('./content/first.txt').then(result => {
+    //     console.log(result);
+// }).catch((err) => {
+// console.log(err);
+// }) 
